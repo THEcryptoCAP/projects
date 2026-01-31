075c666da9455ad139b3a4d6ace330a08bd249b7
THEcryptoCAP <cryptocapinoid@gmail.com>
2026-01-31 20:21:33 +0530

    cargo initialization

---
+++ hello_cargo/target/debug/deps/hello_cargo-a5e885cea2e48412.d
@@ -0,0 +1,5 @@
+/Users/avinash/Desktop/projects/hello_cargo/target/debug/deps/hello_cargo-a5e885cea2e48412.d: src/main.rs
+
+/Users/avinash/Desktop/projects/hello_cargo/target/debug/deps/hello_cargo-a5e885cea2e48412: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/avinash/Desktop/projects/hello_cargo/target/debug/hello_cargo: /Users/avinash/Desktop/projects/hello_cargo/src/main.rs
